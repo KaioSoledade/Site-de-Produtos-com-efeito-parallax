030707b959f5a11906fd35bfdaf2c9044b4e8e90
Kaio Soledade <soledadekaio@gmail.com>
2023-05-12 17:57:50 -0300

    JavaScript colocado

---
+++ script.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal ('.we-are-creatives', {durantion: 10000});
+
+sr.reveal ('.revell', 
+    {rotate: {x: 100, y: 0, z: 1000},
+    durantion: 2000});
+
+
+
+
+sr.reveal ('.revell-type1', {durantion: 200000});
+
+
+
+
+/*
+sr.reveal ('.bloco-menor1', {
+    rotate: {x: 0, y: 80, z: 0},
+    durantion: 2000});
+
+sr.reveal ('.bloco-menor3', 
+    {rotate: {x: 100, y: 0, z: 0},
+    durantion: 2000});
+*/
